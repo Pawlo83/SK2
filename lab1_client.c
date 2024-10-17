42e59119f5f5ed1990bce846e905b8ef54ff2369
Pawlo83 <94640915+Pawlo83@users.noreply.github.com>
2024-10-17 13:37:00 +0200

    Create lab1_client.c

---
+++ lab1_client.c
@@ -0,0 +1,29 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void main(int argc, char* argv[]){
+  struct hostent* hostt;
+  hostt = gethostbyname(argv[1]);
+  
+  int sfd, rc;
+  char buf[256];
+  struct sockaddr_in saddr;
+  sfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+  memset(&saddr, 0, sizeof(saddr));
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(atoi(argv[2]));
+  //saddr.sin_addr.s_addr = inet_addr(argv[1]);
+  
+  memcpy(&saddr.sin_addr.s_addr, hostt->h_addr, hostt->h_length);
+  
+  connect(sfd, (struct sockaddr*) &saddr, sizeof(saddr));
+  rc = read(sfd, buf, sizeof(buf));
+  write(1, buf, rc);
+  close(sfd);
+}
